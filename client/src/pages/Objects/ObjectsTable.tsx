d040bd886eb053127568f37c2a9231631fe65b6e
Alexey Vymyatnin <avymiatnin@aspirity.com>
2024-10-20 12:57:03 +0700

    Fix some styles for object table. Add tags for object type

--- client/src/pages/Objects/ObjectsTable.tsx
+++ client/src/pages/Objects/ObjectsTable.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Table } from 'antd';
+import { Table, Tag } from 'antd';
 import { ColumnsType } from 'antd/es/table';
 import './ObjectsTable.scss';
 import { ObjectType } from '../../types';
@@ -9,16 +9,29 @@ const columns: ColumnsType<ObjectType> = [
     title: 'ID',
     dataIndex: 'object_id',
     key: 'object_id',
+    width: '5%',
   },
   {
     title: 'Имя',
     dataIndex: 'object_name',
     key: 'object_name',
+    width: '20%',
   },
   {
     title: 'Тип',
     dataIndex: 'object_type',
     key: 'object_type',
+    width: '20%',
+    render: (_, { object_type }) => {
+      switch (object_type) {
+        case 'EMS':
+          return <Tag color="green">EMS</Tag>;
+        case 'Network Node':
+          return <Tag color="cyan">Network Node</Tag>;
+        case 'Data Element SNMP':
+          return <Tag color="blue">Data Element SNMP</Tag>;
+      }
+    },
   },
   {
     title: 'Описание',
